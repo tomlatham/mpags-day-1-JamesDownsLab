4c92f4caa813e9ed68ae2b99f1df02738feaa8c7
James Downs <james.downs@nottingham.ac.uk>
2019-10-11 14:33:22 +0100

    Exercise 3

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 int main()
 {
 	int a{5};
@@ -8,7 +9,18 @@ int main()
 	double b{5.5};
 	std::cout << b << std::endl;
 
-	int c{b};
+	int c{3};
 	std::cout << c << std::endl;	
+
+	const double d{7.2};
+	std::cout << d << std::endl;
+
+	int e{7};
+	e = 5;
+
+	std::string msg {"Hello"};
+	std::cout << msg << "\n";
+
+	return 0;
 }
 
