9698ca17458798734eaa1426b90771994598bfac
James Downs <jamesdownslab@gmail.com>
2019-10-16 17:08:58 +0100

    Improved command line parsing

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -3,39 +3,46 @@
 #include <string>
 int main(int argc, char* argv[])
 {
+	const std::string version_number{"0.0.1"};
 	const std::vector<std::string> cmdLineArgv{ argv, argv+argc};
-	const std::string input{cmdLineArgv[1]};
-
-	bool print_help{false};
 	
+	// Variables for arguments
+	std::string input{""};
+
+	// Variables for flags
+	bool print_help{false};
 	bool print_version{false};
 
-	int output_loc;
-	bool output_given{false};
-	int input_file_loc;
-	bool input_file_given{false};
+	// Variables for options
+	std::string output_file;
+	std::string input_file;
 
 	// start loop at 1 to ignore program name
 	for (size_t i{1}; i < cmdLineArgv.size(); i++)
 	{
-		// std::cout << cmdLineArgv[i] << "\n";	// This line prints each command line argument
 
+		// Test flags
 		if (cmdLineArgv[i] == "-h" || cmdLineArgv[i] == "--help"){
 			print_help = true;
 		}
 
-		if (cmdLineArgv[i] == "--version"){
+		else if (cmdLineArgv[i] == "--version"){
 			print_version = true;
 		}
 
-		if (cmdLineArgv[i] == "-o"){
-			output_loc = i + 1;
-			output_given = true;
+		// Test options
+		else if (cmdLineArgv[i] == "-o"){
+			i++;
+			output_file = cmdLineArgv[i];
 		}
 
-		if (cmdLineArgv[i] == "-i"){
-			input_file_loc = i + 1;
-			input_file_given = true;
+		else if (cmdLineArgv[i] == "-i"){
+			i++;
+			input_file = cmdLineArgv[i];
+		}
+
+		else {
+			input = cmdLineArgv[i];
 		}
 	}	
 
@@ -47,73 +54,63 @@ int main(int argc, char* argv[])
 		std::cout << "Version: 0.0.1" << "\n";
 	}
 
-	std::string output_filename;
-	if (output_given == true){
-		output_filename = cmdLineArgv[output_loc];
-		std::cout << "Output File is " << output_filename << "\n";
-	}
-
-	std::string input_filename;
-	if (input_file_given == true){
-		input_filename = cmdLineArgv[input_file_loc];
-		std::cout << "Input File is " << input_filename << "\n";
-	}
-		
-
 	std::string output;
 	char in_char('x');
 	char out_char;
-	for (size_t i{0}; i<input.size(); i++)
-	{
-		in_char = input[i];
-		if (std::isalnum(in_char) != 0)
-		{;
-			switch (in_char)
-			{
-				case '0':
-					output = output + "ZERO";
-					break;
-				
-				case '1':
-					output = output + "ONE";
-					break;
-				case '2':
-					output = output + "TWO";
-					break;
 
-				case '3':
-					output = output + "THREE";
-					break;
+	if (input != ""){
+		for (size_t i{0}; i<input.size(); i++)
+		{
+			in_char = input[i];
+			if (std::isalnum(in_char) != 0)
+			{;
+				switch (in_char)
+				{
+					case '0':
+						output = output + "ZERO";
+						break;
+					
+					case '1':
+						output = output + "ONE";
+						break;
+					case '2':
+						output = output + "TWO";
+						break;
 
-				case '4':
-					output = output + "FOUR";
-					break;
+					case '3':
+						output = output + "THREE";
+						break;
 
-				case '5':
-					output = output + "FIVE";
-					break;
+					case '4':
+						output = output + "FOUR";
+						break;
 
-				case '6':
-					output = output + "SIX";
-					break;
+					case '5':
+						output = output + "FIVE";
+						break;
 
-				case '7':
-					output = output + "SEVEN";
-					break;
+					case '6':
+						output = output + "SIX";
+						break;
 
-				case '8':
-					output = output + "EIGHT";
-					break;
+					case '7':
+						output = output + "SEVEN";
+						break;
 
-				case '9':
-					output = output + "NINE";
-					break;
+					case '8':
+						output = output + "EIGHT";
+						break;
 
-				default:
-	
-					out_char = std::toupper(in_char);
-					output = output + out_char;
-					break;
+					case '9':
+						output = output + "NINE";
+						break;
+
+					default:
+		
+						out_char = std::toupper(in_char);
+						output = output + out_char;
+						break;
+				}
 			}
 		}
 	}
